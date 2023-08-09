944ecfc12fa346faf8ba7beae1db5c3d3fef6790
BenThere <benjaminbirdsall@icloud.com>
2023-08-09 17:32:46 -0600

    added api

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,11 @@ const num = true;
 const char = true;
 const caps = true;
 const len = 18; // min 7
-const apiKey = "f403ba2a0a0342f3a0385bda04013303"
+const apiKey = '33aaa9c75b944b7ca0b99e6c558d4ee3';
+
+
+// GET https://haveibeenpwned.com/api/v3/breachedaccount/{account}
+// hibp-api-key: ["f403ba2a0a0342f3a0385bda04013303"];
 
 const fetchUrl = `https://passwordinator.onrender.com?num=${num}&char=${char}&caps=${caps}&len=${len}`;
 
@@ -19,3 +23,31 @@ fetch(fetchUrl)
   .catch((error) => {
     console.error('Fetch error:', error);
   });
+
+  ////////////////////////
+
+// const apiKey = "YOUR_API_KEY"; // Replace with your actual API key
+const domain = "facebook.com";
+
+const apiUrl = `https://haveibeenpwned.com/api/v3/breacheddomain/${domain}`;
+
+fetch(apiUrl, {
+  headers: {
+    "User-Agent": "YourAppName", // Replace with your app's name or identifier
+    "hibp-api-key": apiKey
+  }
+})
+.then(response => {
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error("Domain not found in breaches");
+  }
+})
+.then(data => {
+  console.log("Breaches:", data);
+})
+.catch(error => {
+  console.error("Error:", error);
+});
+  
