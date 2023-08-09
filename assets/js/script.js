fef7a616a1522796ef85a0c40ce021679d307e80
Brielle <brielleb.broadt@gmail.com>
2023-08-09 10:10:58 -0500

    added API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const num = true;
 const char = true;
 const caps = true;
 const len = 18; // min 7
+const apiKey = "f403ba2a0a0342f3a0385bda04013303"
 
 const fetchUrl = `https://passwordinator.onrender.com?num=${num}&char=${char}&caps=${caps}&len=${len}`;
 
