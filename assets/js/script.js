dd42991e0b11bacf1cbaff3aec651c6aaa29ccfc
BenThere <benjaminbirdsall@icloud.com>
2023-08-10 22:50:41 -0600

    added init and extra memes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 var button = document.getElementById("password-button");
 var newPassword = document.getElementById("new-password");
+var memeEl = document.getElementById("checkbox-meme");
+var num = document.getElementById('num');
+var char = document.getElementById('char');
+var caps = document.getElementById('caps');
 
+// Password API
 button.addEventListener('click', function() {
-  const fetchUrl = `https://passwordinator.onrender.com?num=true&char=true&caps=true&len=18`;
+  const fetchUrl = 'https://passwordinator.onrender.com?num=true&char=true&caps=true&len=18';
   var password = "";
   fetch(fetchUrl)
     .then((response) => {
@@ -18,6 +23,45 @@ button.addEventListener('click', function() {
     .catch((error) => {
       console.error('Fetch error:', error);
     });
+})
+
+num.addEventListener('click', function() {
+  memeEl.style.display = 'block';
+  if (num.checked + char.checked + caps.checked == 1) {
+    memeEl.setAttribute("src", './assets/images/it-doesnt-do-anything.jpeg');
+  } else if (num.checked + char.checked + caps.checked == 2) {
+    memeEl.setAttribute("src", './assets/images/are-you-listening.gif');
+  } else if (num.checked + char.checked + caps.checked == 3) {
+    memeEl.setAttribute("src", './assets/images/you-have-no-power-here.jpeg');
+  } else {
+    memeEl.style.display = 'none';
+  }
+})
+
+char.addEventListener('click', function() {
+  memeEl.style.display = 'block';
+  if (num.checked + char.checked + caps.checked == 1) {
+    memeEl.setAttribute("src", './assets/images/it-doesnt-do-anything.jpeg');
+  } else if (num.checked + char.checked + caps.checked == 2) {
+    memeEl.setAttribute("src", './assets/images/are-you-listening.gif');
+  } else if (num.checked + char.checked + caps.checked == 3) {
+    memeEl.setAttribute("src", './assets/images/you-have-no-power-here.jpeg');
+  } else {
+    memeEl.style.display = 'none';
+  }
+})
+
+caps.addEventListener('click', function() {
+  memeEl.style.display = 'block';
+  if (num.checked + char.checked + caps.checked == 1) {
+    memeEl.setAttribute("src", './assets/images/it-doesnt-do-anything.jpeg');
+  } else if (num.checked + char.checked + caps.checked == 2) {
+    memeEl.setAttribute("src", './assets/images/are-you-listening.gif');
+  } else if (num.checked + char.checked + caps.checked == 3) {
+    memeEl.setAttribute("src", './assets/images/you-have-no-power-here.jpeg');
+  } else {
+    memeEl.style.display = 'none';
+  }
 })
 
 // Joke API
@@ -40,3 +84,9 @@ fetch(apiUrl)
     console.error('An error occurred:', error);
   });
 });
+
+function init() {
+  memeEl.style.display = 'none';
+}
+
+init();
